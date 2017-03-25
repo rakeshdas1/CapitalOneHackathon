ad49aa8b6834cbfc5f2e626771b584a8bbc3dc78
Rakesh Das <rakeshdaskowshik@gmail.com>
2017-03-25 14:47:02 -0500

    form finished

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+var router = express.Router(); 
 
 app.set('view engine', 'ejs');
 
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
 app.get('/', function(req, res){
     res.render('routes/index');
+});
+
+app.post('/submit', function(req, res){
+    console.log(req.body);
+    res.render('routes/submitted')
 });
 
 
